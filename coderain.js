1a321e4a90996511119047562d1320babd5b1df3
Chris Karcher <cmkarcher1@gmail.com>
2017-11-18 19:09:43 -0500

    Back to original javascript

--- coderain.js
+++ coderain.js
@@ -1,8 +1,7 @@
-// JavaScript source code
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+var canvas = document.getElementById(‘canvas’);
+var ctx = canvas.getContext(‘2d’);
 var t = text();
-var logo = document.getElementById("logo");
+var logo = document.getElementById(‘logo’);
 var lines = [];
 window.setInterval(draw, 100);
 function draw() {
@@ -13,6 +12,7 @@ function draw() {
     lines.forEach(function(tl) {
         ctx.drawImage(tl.text, tl.posX, tl.animate(), 20, 1000);
     });
+    ctx.drawImage(logo, 100, 155, 400, 70);
 }
 function textLine() {
     this.text = t;
@@ -29,19 +29,19 @@ function textLine() {
     };
 }
 function text() {
-    var offscreenCanvas = document.createElement("canvas");
-    offscreenCanvas.width = "30";
-    offscreenCanvas.height = "1000";
-    offscreenCanvas.style.display = "none";
+    var offscreenCanvas = document.createElement(‘canvas’);
+    offscreenCanvas.width = “30”;
+    offscreenCanvas.height = “1000”;
+    offscreenCanvas.style.display = “none”;
     document.body.appendChild(offscreenCanvas);
-    var octx = offscreenCanvas.getContext("2d");
-    octx.textAlign = "center";
-    octx.shadowColor = "lightgreen";
+    var octx = offscreenCanvas.getContext(‘2d’);
+    octx.textAlign = “center”;
+    octx.shadowColor = “lightgreen”;
     octx.shadowOffsetX = 2;
     octx.shadowOffsetY = -5;
     octx.shadowBlur = 1;
-    octx.fillStyle = "darkgreen";
-    octx.textAlign = "left";
+    octx.fillStyle = ‘darkgreen’;
+    octx.textAlign = “left”;
     var step = 10;
     for (i = 0; i < 100; i++) {
         var charCode = 0;
